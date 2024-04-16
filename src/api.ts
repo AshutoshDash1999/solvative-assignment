c3122fb7c855567ea79ebde0f3ecb72e25f0db8f
Ashutosh Dash <dashashutosh1999@gmail.com>
2024-04-16 21:16:16 +0530

    refactor: api client for fetching city details

--- src/api.ts
+++ src/api.ts
@@ -1,19 +1,37 @@
 import axios from "axios";
 
-const apiRequest = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    "X-RapidAPI-Key": import.meta.env.VITE_API_KEY,
-    "X-RapidAPI-Host": import.meta.env.VITE_API_HOST,
-  },
-});
+interface FetchCityInfoProps {
+  countryId: string;
+  namePrefix: string;
+  limit: number | string;
+}
 
-apiRequest.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    console.error("Error while fetching the API:", error);
-    throw error;
+const fetchCityInfo = async (
+  countryId: string,
+  namePrefix: string,
+  limit: string
+) => {
+  const options = {
+    method: "GET",
+    url: import.meta.env.VITE_API_URL,
+    params: {
+      countryIds: countryId,
+      namePrefix: namePrefix,
+      limit: limit,
+    },
+    headers: {
+      "x-rapidapi-host": import.meta.env.VITE_API_HOST,
+      "x-rapidapi-key": import.meta.env.VITE_API_KEY,
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    console.error("Error while fetching cities:", error);
+    throw error; // Rethrow the error to be handled by the caller
   }
-);
+};
 
-export default apiRequest;
+export default fetchCityInfo;
