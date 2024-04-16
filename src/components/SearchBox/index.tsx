22181878dca1bf9006ae548f65e76f341fd54dd5
Ashutosh Dash <dashashutosh1999@gmail.com>
2024-04-16 22:34:54 +0530

    feat: ui changes on key press

--- src/components/SearchBox/index.tsx
+++ src/components/SearchBox/index.tsx
@@ -1,18 +1,49 @@
-import fetchCityInfo from "../../api";
+import { useEffect, useRef } from "react";
 import KeyboardShortcut from "../KeyboardShortcut";
 
-const SearchBox = (props) => {
-  async function getCities() {
-    try {
-      const cities = await fetchCityInfo("IN", "del", "5");
-      console.log(cities);
-    } catch (error) {
-      console.error("Failed to fetch cities:", error);
+const SearchBox = () => {
+  //    const  getCities = () => {
+  //     try {
+  //       const cities = await fetchCityInfo("IN", "del", "5");
+  //       console.log(cities);
+  //     } catch (error) {
+  //       console.error("Failed to fetch cities:", error);
+  //     }
+  //   }
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const handleKeyDown = (event: globalThis.KeyboardEvent) => {
+      if (event.ctrlKey && event.key === "/") {
+        event.preventDefault();
+        inputRef.current?.focus();
+
+        console.log("key pressed");
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  const searchCityHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      console.log("Enter key pressed");
     }
-  }
+  };
+
   return (
     <div className="searchbox__container">
-      <input placeholder="Search places..." className="searchbox__input" />
+      <input
+        placeholder="Search places..."
+        className="searchbox__input"
+        ref={inputRef}
+        onKeyDown={searchCityHandler}
+      />
       <KeyboardShortcut>Ctrl + /</KeyboardShortcut>
     </div>
   );
